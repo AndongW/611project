a3158ccedd0a90a0c1b154d722428ac29a48162d
andong <andong@bios611-18.bios.unc.edu>
2023-10-25 18:07:21 -0400

    Corrected file directories.

--- make_plots.R
+++ make_plots.R
@@ -1,12 +1,15 @@
+# Load Libraries
+library(tidyverse)
+
 # Read in data
-album_rate <- read_csv("./work/611project/data/album_rate.csv")
-album_rev <- read_csv("./work/611project/data/album_rev.csv")
+album_rate <- read_csv("./data/album_rate.csv")
+album_rev <- read_csv("./data/album_rev.csv")
 
 # Plot 1
 rate_barplot <- ggplot(album_rate, aes(x=`Release Year`)) +
   geom_bar()
 
-ggsave("./work/611project/figures/rate_barplot.png",plot =rate_barplot)
+ggsave("./figures/rate_barplot.png",plot =rate_barplot)
 
 # Plot 2
 cbPalette <- c("#000000", "#E69F00", "#56B4E9", "#F0E442", "#009E73", "#0072B2", "#D55E00", "#CC79A7")
@@ -15,4 +18,4 @@ rev_barplot <- ggplot(album_rev, aes(x=Rating, fill = Reception)) +
   geom_histogram(binwidth = 1) +
   scale_fill_manual(values=cbPalette)
 
-ggsave("./work/611project/figures/rev_barplot.png",plot =rev_barplot)
+ggsave("./figures/rev_barplot.png",plot =rev_barplot)
--- prep.R
+++ prep.R
@@ -2,12 +2,12 @@
 library(tidyverse)
 
 # Data Preprocessing
-album_rate <- read_csv("./work/611project/source_data/albums/album_ratings.csv")
-album_rev1 <- read_csv("./work/611project/source_data/albums/album_reviews/train.csv")
-album_rev2 <- read_csv("./work/611project/source_data/albums/album_reviews/test.csv")
+album_rate <- read_csv("./source_data/albums/album_ratings.csv")
+album_rev1 <- read_csv("./source_data/albums/album_reviews/train.csv")
+album_rev2 <- read_csv("./source_data/albums/album_reviews/test.csv")
 album_rev <- rbind(album_rev1,album_rev2)
 
 # Export data
-write.csv(album_rate,"./work/611project/data/album_rate.csv")
-write.csv(album_rev,"./work/611project/data/album_rev.csv")
+write.csv(album_rate,"./data/album_rate.csv")
+write.csv(album_rev,"./data/album_rev.csv")
 
