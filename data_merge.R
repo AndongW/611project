9d3f9ee3f57ff4f52e495b79b60b5619602dd5d5
andong <andong@bios611-18.bios.unc.edu>
2023-12-13 13:44:26 -0500

    Fixed data merging variable name mistakes.

--- data_merge.R
+++ data_merge.R
@@ -6,13 +6,13 @@ rate <- read_csv("./work/data/album_rate.csv")
 rev <- read_csv("./work/data/album_rev.csv")
 
 # Merge/join the two datasets based on artist+title
-rate_merge <- rate_score %>%
+rate_merge <- rate %>%
   mutate(AT = paste(Artist,Title, sep="<"))
-rev_merge <- rev_piv %>%
+rev_merge <- rev %>%
   mutate(AT = paste(Artist,Title, sep="<")) %>%
   select(-c(Artist,Title))
 albums <-  inner_join(rate_merge,rev_merge, by = "AT") %>%
   select(-AT)
 
 # Export data
-write_csv(rev_piv,"./work/data/album.csv")
+write_csv(albums,"./work/data/album.csv")
