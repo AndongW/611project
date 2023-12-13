8d7ed311ec9c4848a220f8dc44ab5e9aee1c7e1e
andong <andong@bios611-18.bios.unc.edu>
2023-12-13 13:36:59 -0500

    Comment and format year.R file.

--- year.R
+++ year.R
@@ -1,30 +1,38 @@
 # Load Libraries
 library(tidyverse)
 
-# Read data 
+# Read data
 albums <- read_csv("./work/data/album.csv")
 
-# 
-composite_score <- rowMeans(albums[,20:152],na.rm = TRUE)
+# Make composite score based on all scores
+composite_score <- rowMeans(albums[, 20:152], na.rm = TRUE)
 albums <- albums %>%
   mutate(composite_score = composite_score)
 
-ggplot(albums, aes(x=`Release Year`,y=composite_score,col= genre_type)) +
-  geom_jitter() 
-
+# Plot pop albums composite scores by year
 pop <- albums %>%
-  filter(genre_type=="Pop")
-ggplot(pop, aes(x=`Release Year`,y=composite_score,fill = as.factor(`Release Year`))) +
+  filter(genre_type == "Pop")
+p1 <- ggplot(pop, 
+             aes(
+               x = `Release Year`,
+               y = composite_score,
+               fill = as.factor(`Release Year`)
+)) +
   geom_boxplot() +
-  scale_y_continuous(name="Composite Score", limits = c(0,100)) +
+  scale_y_continuous(name = "Composite Score", limits = c(0, 100)) +
   ggtitle("Pop: Composite Score over the Years") +
-  theme(legend.position="none")
+  theme(legend.position = "none")
 
+# Plot rock albums composite scores by year
 rock <- albums %>%
-  filter(genre_type=="Rock")
-ggplot(rock, aes(x=`Release Year`,y=composite_score,fill = as.factor(`Release Year`))) +
+  filter(genre_type == "Rock")
+p2 <- ggplot(rock,
+             aes(
+               x = `Release Year`,
+               y = composite_score,
+               fill = as.factor(`Release Year`)
+             )) +
   geom_boxplot() +
-  scale_y_continuous(name="Composite Score",limits = c(0,100)) +
+  scale_y_continuous(name = "Composite Score", limits = c(0, 100)) +
   ggtitle("Rock: Composite Score over the Years") +
-  theme(legend.position="none")
-
+  theme(legend.position = "none")
