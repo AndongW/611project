0e342e7a1860ddfd1b407e2692b12f71f4b58c4d
andong <andong@bios611-18.bios.unc.edu>
2023-12-13 03:25:31 -0500

    Replicate overlay density plot for different settings.

--- critic_user.R
+++ critic_user.R
@@ -32,3 +32,110 @@ p1 <-
   ggtitle("Metacritic Adjusted Score: Pop vs. Rock") +
   theme_minimal()
 p1
+
+# Plot AOTY Adjusted Score: Pop vs. Rock
+df_p2 <- data.frame(
+  value = c(
+    rate_pop$aoty_adj_score,
+    rate_rock$aoty_adj_score
+  ),
+  Genre_group = rep(c("1.Pop", "2.Rock"),
+                    times = c(
+                      length(rate_pop$aoty_adj_score),
+                      length(rate_rock$aoty_adj_score)
+                    ))
+)
+p2 <-
+  ggplot(df_p2, aes(x = value, fill = Genre_group, color = Genre_group)) +
+  geom_density(alpha = 0.1) +
+  scale_x_continuous(name = "AOTY Adjusted Score", breaks = seq(0,100,25), limits = c(0,100)) +
+  scale_y_continuous(name = "Density") +
+  ggtitle("AOTY Adjusted Score: Pop vs. Rock") +
+  theme_minimal()
+p2
+
+# Plot Metacritic Critic Score
+df_p3 <- data.frame(
+  value = c(
+    rate_pop$`Metacritic Critic Score`,
+    rate_rock$`Metacritic Critic Score`
+  ),
+  Genre_group = rep(c("1.Pop", "2.Rock"),
+                    times = c(
+                      dim(rate_pop)[1],
+                      dim(rate_rock)[1]
+                    ))
+)
+p3 <-
+  ggplot(df_p3, aes(x = value, fill = Genre_group, color = Genre_group)) +
+  geom_density(alpha = 0.1) +
+  scale_x_continuous(name = "Metacritic Critic Score", breaks = seq(0,100,25), limits = c(0,100)) +
+  scale_y_continuous(name = "Density") + 
+  ggtitle("Metacritic Critic Score: Pop vs. Rock") +
+  theme_minimal()
+p3
+
+# Plot AOTY Critic Score
+df_p4 <- data.frame(
+  value = c(
+    rate_pop$`AOTY Critic Score`,
+    rate_rock$`AOTY Critic Score`
+  ),
+  Genre_group = rep(c("1.Pop", "2.Rock"),
+                    times = c(
+                      dim(rate_pop)[1],
+                      dim(rate_rock)[1]
+                    ))
+)
+p4 <-
+  ggplot(df_p4, aes(x = value, fill = Genre_group, color = Genre_group)) +
+  geom_density(alpha = 0.1) +
+  scale_x_continuous(name = "AOTY Critic Score", breaks = seq(0,100,25), limits = c(0,100)) +
+  scale_y_continuous(name = "Density") + 
+  ggtitle("AOTY Critic Score: Pop vs. Rock") +
+  theme_minimal()
+p4
+
+# Plot Metacritic User Score
+df_p5 <- data.frame(
+  value = c(
+    rate_pop$`Metacritic User Score`,
+    rate_rock$`Metacritic User Score`
+  ),
+  Genre_group = rep(c("1.Pop", "2.Rock"),
+                    times = c(
+                      dim(rate_pop)[1],
+                      dim(rate_rock)[1]
+                    ))
+)
+p5 <-
+  ggplot(df_p5, aes(x = value, fill = Genre_group, color = Genre_group)) +
+  geom_density(alpha = 0.1) +
+  scale_x_continuous(name = "Metacritic User Score", breaks = seq(0,100,25), limits = c(0,100)) +
+  scale_y_continuous(name = "Density") + 
+  ggtitle("Metacritic User Score: Pop vs. Rock") +
+  theme_minimal()
+p5
+
+# Plot AOTY User Score
+df_p6 <- data.frame(
+  value = c(
+    rate_pop$`AOTY User Score`,
+    rate_rock$`AOTY User Score`
+  ),
+  Genre_group = rep(c("1.Pop", "2.Rock"),
+                    times = c(
+                      dim(rate_pop)[1],
+                      dim(rate_rock)[1]
+                    ))
+)
+p6 <-
+  ggplot(df_p6, aes(x = value, fill = Genre_group, color = Genre_group)) +
+  geom_density(alpha = 0.1) +
+  scale_x_continuous(name = "AOTY User Score", breaks = seq(0,100,25), limits = c(0,100)) +
+  scale_y_continuous(name = "Density") + 
+  ggtitle("AOTY User Score: Pop vs. Rock") +
+  theme_minimal()
+p6
+
+
